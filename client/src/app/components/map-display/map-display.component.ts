b40c1f02ab34d4d22f27322592db1652030365a9
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-12 17:15:51 -0400

    component: map-display

---
+++ client/src/app/components/map-display/map-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-display.component.html',
+  styleUrl: './map-display.component.scss'
+})
+export class MapDisplayComponent {
+
+}
