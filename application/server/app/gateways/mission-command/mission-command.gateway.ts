e5a3fb25386cc37c900ebd7678071be601d2c620
Ely Cheikh Abass <ely-cheikh.abass@polymtl.ca>
2024-09-13 15:43:56 -0400

    fix: publisher subscriber using ros bridge

---
+++ application/server/app/gateways/mission-command/mission-command.gateway.ts
@@ -0,0 +1,10 @@
+import { RobotService } from '@app/services/robot/robot.service';
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class MissionCommandGateway {
+    private readonly logger = new Logger(MissionCommandGateway.name);
+    private simulator = new RobotService(process.env.SIMULATION_IP);
+    constructor() {
+    }
+}
