a1bbfc0c2c0fd738e5b709be3ec1b4f6edf3866f
Ely Cheikh Abass <ely-cheikh.abass@polymtl.ca>
2024-09-13 15:43:32 -0400

    feat: publish circle drawing to robot

--- embedded_ws/src/com_bridge/com_bridge/com_serv.py
+++ embedded_ws/src/com_bridge/com_bridge/com_serv.py
@@ -4,13 +4,11 @@ import rclpy
 from rclpy.node import Node
 from common_msgs.msg import StartMission
 from std_msgs.msg import String
-from com_bridge.socket_manager import SocketManager
 import json
 
 class ComServNode(Node):
     def __init__(self):
         super().__init__('com_serv')
-        self.socket_manager = SocketManager()
 
         # Abonnement au topic pour les messages de mission
         self.mission_subscription = self.create_subscription(
@@ -20,36 +18,36 @@ class ComServNode(Node):
             10
         )
 
-    def server_info_callback(self, msg):
-        server_info = msg.data.split(',')
-        if len(server_info) == 2:
-            server_ip = server_info[0]
-            port = int(server_info[1])
-            self.socket_manager.connect(server_ip, port)
-            self.get_logger().info(f'Connected to server at {server_ip}:{port}')
-            # print(f'Connected to server at {server_ip}:{port}')
+        self.start_mission_order = self.create_publisher(geometry_msgs.msg.Twist, 'cmd_vel', 10)
+        # msg_to_send = String()
+        # msg_to_send.data = 'Ok'
+        # self.update.publish(msg_to_send)
 
     def start_mission_callback(self, msg):
-        # Convertir le message ROS en JSON
-        # self.get_logger().info(msg)
-        # print(type(msg))
         json_data = {
             "command": msg.command,
-            "orientation": msg.MissionDetails.orientation,
+            "orientation": msg.mission_details.orientation,
             "position": {
-                "x": msg.MissionDetails.position.x,
-                "y": msg.MissionDetails.position.y
+                "x": msg.mission_details.position.x,
+                "y": msg.mission_details.position.y
             },
             "timestamp": msg.timestamp
         }
         json_str = json.dumps(json_data)
         self.get_logger().info(f'Sending message: {json_str}')
 
-        # Envoyer le JSON au serveur distant
-        # self.socket_manager.send(json_str)
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.1
+        twist_msg.linear.y = 0.0
+        twist_msg.linear.z = 0.0
+        twist_msg.angular.x = 0.0
+        twist_msg.angular.y = 0.0
+        twist_msg.angular.z = 1.8
+        
+        self.start_mission_order.publish(twist_msg)
+
 
     def destroy_node(self):
-        self.socket_manager.close()
         super().destroy_node()
 
 def main(args=None):
