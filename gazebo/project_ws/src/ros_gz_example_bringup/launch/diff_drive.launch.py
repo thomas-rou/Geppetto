ef29cd9def7f8615ca62ef8bd29cfd471575a4ce
Abdul Chaarani <abdul@chaarani.ca>
2024-09-14 23:39:31 -0400

    test: hardcode 2 sim robots

--- gazebo/project_ws/src/ros_gz_example_bringup/launch/diff_drive.launch.py
+++ gazebo/project_ws/src/ros_gz_example_bringup/launch/diff_drive.launch.py
@@ -82,6 +82,13 @@ def generate_launch_description():
         output='screen'
     )
 
+    # ros2 launch ros_gz_sim ros_gz_spawn_model.launch.py world:=empty file:=$(ros2 pkg prefix --share ros_gz_sim_demos)/models/vehicle/model.sdf name:=my_vehicle x:=5.0 y:=5.0 z:=0.5
+
+    robot2 = Node(
+        package='ros_gz_sim',
+        
+    )
+
     return LaunchDescription([
         gz_sim,
         # DeclareLaunchArgument('rviz', default_value='true', description='Open RViz.'),
