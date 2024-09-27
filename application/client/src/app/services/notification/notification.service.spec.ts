3341c7fdc3ea0e03ec031536454095745ab4c642
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-27 18:10:28 -0400

    feat: notification service tests

--- application/client/src/app/services/notification/notification.service.spec.ts
+++ application/client/src/app/services/notification/notification.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
 import { NotificationService } from './notification.service';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 describe('NotificationService', () => {
   let service: NotificationService;
@@ -12,5 +13,20 @@ describe('NotificationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send notification', (done) => {
+    const testMessage = 'Test notification';
+    service.getNotifications().pipe(take(1)).subscribe((message) => {
+      expect(message).toBe(testMessage);
+      done();
+    });
+    service.sendNotification(testMessage);
+  });
+
+  it('should return notifications as observable', () => {
+    const notifications$ = service.getNotifications();
+    expect(notifications$).toBeTruthy();
+    expect(notifications$).toBeInstanceOf(Observable);
   });
 });
