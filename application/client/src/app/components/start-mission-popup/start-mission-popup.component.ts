b84e7b67a2a43e66395fa7739365fd4a79ecc6dd
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-27 13:18:52 -0400

    feat: popup logic

--- application/client/src/app/components/start-mission-popup/start-mission-popup.component.ts
+++ application/client/src/app/components/start-mission-popup/start-mission-popup.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Robot } from '@app/classes/robot';
+import { RobotManagementService } from '@app/services/robot-management/robot-management.service';
 
 @Component({
   selector: 'app-start-mission-popup',
-  standalone: true,
-  imports: [],
   templateUrl: './start-mission-popup.component.html',
-  styleUrl: './start-mission-popup.component.scss'
+  styleUrls: ['./start-mission-popup.component.scss'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class StartMissionPopupComponent {
+  robot1X: number = 0;
+  robot1Y: number = 0;
+  robot1Orientation: number = 0.0;
+  robot2X: number = 0;
+  robot2Y: number = 0;
+  robot2Orientation: number = 0.0;
 
+  constructor(
+    private managementService: RobotManagementService
+  ) {}
+
+  get robot1() { return this.managementService.robot1; }
+  get robot2() { return this.managementService.robot2; }
+
+  @Output() startMission = new EventEmitter<{ robot1: Robot, robot2: Robot }>();
+
+  onStartMission() {
+    this.robot1.position = { x: this.robot1X, y: this.robot1Y };
+    this.robot2.position = { x: this.robot2X, y: this.robot2Y };
+    
+    this.robot1.orientation = this.robot1Orientation;
+    this.robot2.orientation = this.robot2Orientation;
+
+    this.startMission.emit();
+  }
 }
