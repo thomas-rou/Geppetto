41fe33d6c9cace3111b55f3a69c59ed878c838f9
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-27 15:24:14 -0400

    feat: robot-management service implementation

---
+++ application/client/src/app/services/robot-management/robot-management.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Robot } from '@app/classes/robot';
+import { RobotStatus } from '@app/enums/robot-status';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class RobotManagementService {
+    robot1: Robot = new Robot('1', 'Robot 1', RobotStatus.Idle, 100, { x: 0, y: 0 }, 0.0);
+    robot2: Robot = new Robot('2', 'Robot 2', RobotStatus.Idle, 100, { x: 0, y: 0 }, 0.0);
+
+    constructor() {}
+}
