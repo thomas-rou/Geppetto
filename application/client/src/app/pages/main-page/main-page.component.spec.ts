3b5ce47e23269b224ff1eff04d486f41b57847ac
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-27 15:22:50 -0400

    fix: import

--- application/client/src/app/pages/main-page/main-page.component.spec.ts
+++ application/client/src/app/pages/main-page/main-page.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { MainPageComponent } from './main-page.component';
-import { ThemeService } from '@app/services/theme.service';
+import { ThemeService } from '@app/services/theme/theme.service';
 import { ControlPanelComponent } from '@app/components/control-panel/control-panel.component';
 import { StatusDisplayComponent } from '@app/components/status-display/status-display.component';
 import { MapDisplayComponent } from '@app/components/map-display/map-display.component';
