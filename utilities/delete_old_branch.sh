1b2524c4688a5c9b7f406c973770c26ddb89bdad
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-09-30 16:55:35 -0400

    feat:create db container

---
+++ utilities/delete_old_branch.sh
@@ -0,0 +1,8 @@
+# Pre-requisite: git-lfs if you encounter error: "This repository is configured for Git LFS..."
+# Run this command: sudo apt install git-lfs
+git for-each-ref --sort=-committerdate --format="%(refname:short) %(committerdate:unix)" refs/remotes/ | awk -v two_weeks_ago=$(date -d '2 weeks ago' +%s) '$2 <= two_weeks_ago {print $1}' | while read branch; do
+    remote_branch=$(echo $branch | sed 's#^origin/##')
+    echo "deleting branch "$remote_branch"..."
+    git push origin --delete "$remote_branch" 2>/dev/null
+
+done
