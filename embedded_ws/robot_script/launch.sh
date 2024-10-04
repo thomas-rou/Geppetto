d4317146b4f217ae430fe57fc79704eee0182949
Loic Nguemegne <loic.nguemegne-temena@polymtl.ca>
2024-10-04 23:39:41 +0000

    build base limo project

--- embedded_ws/robot_script/launch.sh
+++ embedded_ws/robot_script/launch.sh
@@ -1,5 +1,5 @@
 MAIN_BRANCH="develop"
-AUDIO_DEVICE_ID=pactl list short sinks | grep "USB_PnP_Audio_Device" | awk '{print $1}'
+AUDIO_DEVICE_ID=$(pactl list short sinks | grep "USB_PnP_Audio_Device" | awk '{print $1}')
 sudo chown nvidia:nvidia /dev/ttyTHS1 &&
 if [ -d ~/geppetto/ ]; then
     cd ~/geppetto/; git switch $MAIN_BRANCH; git pull
@@ -7,6 +7,7 @@ else
     cd ~; git clone https://github.com/thomas-rou/Geppetto.git
     cd ~/geppetto/; git switch $MAIN_BRANCH; git pull
 fi
+cd ~/limo_ws; colcon build;  source install/setup.bash; 
 cd ~/limo_ws/src/limo_ros2; ros2 launch limo_bringup limo_start.launch.py &
 cd ~/geppetto/embedded_ws/; colcon build;  source install/setup.bash;  ros2 run com_bridge com_serv &
 source install/setup.bash; ros2 run rosbridge_server rosbridge_websocket
