7eb1ac5478177bca08fa010dc26c6087c4cb318a
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-10-04 21:30:49 -0400

    update launch script

--- embedded_ws/robot_script/launch.sh
+++ embedded_ws/robot_script/launch.sh
@@ -10,6 +10,6 @@ else
 fi
 cd ~/limo_ws; colcon build; source install/setup.bash
 cd ~/limo_ws/src/limo_ros2; ros2 launch limo_bringup limo_start.launch.py &
-cd ~/geppetto/embedded_ws/; colcon build;  source install/setup.bash;  ros2 run com_bridge com_serv &
+cd ~/geppetto/embedded_ws/; colcon build;  source install/setup.bash;  ros2 run com_bridge com_serv & ros2 run mission_status_manager mission_manager
 source install/setup.bash; ros2 run rosbridge_server rosbridge_websocket
 pactl set-default-sink $AUDIO_DEVICE_ID
