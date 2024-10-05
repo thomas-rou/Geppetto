406ee466e589555023f229d33aa09f7269b18b7a
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-10-04 20:35:30 -0400

    create mission_status message

--- embedded_ws/src/mission_status_manager/mission_status_manager/mission_manager.py
+++ embedded_ws/src/mission_status_manager/mission_status_manager/mission_manager.py
@@ -1,15 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from limo_msgs.msg import LimoStatus
-from std_msgs.msg import String, Float32
-# from common_msgs.msg import StartMission, StopMission, IdentifyRobot
+from common_msgs.msg import MissionStatus
+BATTERY_CAPACITY = 12.0
+
 import os
 
 class MissionManager(Node):
     def __init__(self):
         super().__init__('mission_manager')
         self.get_logger().info(f"Mission manager Launched waiting for messages in {os.getenv('ROBOT')}")
-        self.mission_status_publisher = self.create_publisher(String,f"{os.getenv('ROBOT')}/mission_status", 10)
+        self.mission_status_publisher = self.create_publisher(MissionStatus,f"{os.getenv('ROBOT')}/mission_status", 10)
 
         self.battery_subscription = self.create_subscription(
             LimoStatus,
@@ -20,13 +21,11 @@ class MissionManager(Node):
 
     def status_publication_callback(self, battery_data):
         try:
-            battery_level = battery_data.voltage
-            battery_msg = Float32()
-            battery_msg.data = battery_level
-            msg = String()
-            msg.data = os.getenv('MISSION_STATUS')
-            self.get_logger().info(f"Publishing: {msg.data}")
-            self.mission_status_publisher.publish(msg)
+            battery_level = round((battery_data.battery_voltage/BATTERY_CAPACITY)*100)
+            mission_status = MissionStatus()
+            mission_status.battery_level = battery_level
+            mission_status.mission_status = os.getenv('MISSION_STATUS')
+            self.mission_status_publisher.publish(mission_status)
         except:
             self.get_logger().info("Failed to publish mission status")
 
