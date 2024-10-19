ba2d5004009c1843e7f2420ccdd3f28d4442b06d
Abdul Chaarani <abdul@chaarani.ca>
2024-10-19 11:26:58 -0400

    fix: undo removal of __init__.py

--- gazebo/project_ws/src/ros_gz_example_bringup/launch/helpers/__init__.py
+++ gazebo/project_ws/src/ros_gz_example_bringup/launch/helpers/__init__.py
@@ -3,6 +3,8 @@ from .config import *
 from .classes.core.entity import Entity
 from .classes.core.pose import Pose
 from .classes.core.size import Size
-from .classes.obstacles.wall import Wall
+from .classes.obstacles.sand_table.sand_table import Sand_Table
+from .classes.obstacles.wall.wall import Wall
+from .classes.obstacles.wall.wall_type import Wall_Type
 from .classes.obstacles.obstacle import Obstacle
 from .classes.robots.robot import Robot
