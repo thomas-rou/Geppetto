2803108279d2ee599cd37eeb9c3f294406b3e28b
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-10-24 19:56:57 -0400

    add list of protected branches

--- utilities/delete_old_branch.sh
+++ utilities/delete_old_branch.sh
@@ -2,8 +2,9 @@
 # Run this command: sudo apt install git-lfs
 git for-each-ref --sort=-committerdate --format="%(refname:short) %(committerdate:unix)" refs/remotes/ | awk -v two_weeks_ago=$(date -d '2 weeks ago' +%s) '$2 <= two_weeks_ago {print $1}' | while read branch; do
     remote_branch=$(echo $branch | sed 's#^origin/##')
-    echo "deleting branch "$remote_branch"..."
-    if [ "$remote_branch" != "main" ] && [ "$remote_branch" != "develop" ]; then
+    protected_branches=("main" "develop" "architecture/mission_action")
+    if [[ ! " ${protected_branches[@]} " =~ " ${remote_branch} " ]]; then
+        echo "deleting branch "$remote_branch"..."
         git push origin --delete "$remote_branch" 2>/dev/null
     fi
 done
