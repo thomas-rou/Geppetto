a0021ed2ddf9bcedae72be86cb04cb4564c3189e
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-10-31 23:42:49 -0400

    test: adujstment

--- application/server/app/gateways/mission-command/mission-command.gateway.spec.ts
+++ application/server/app/gateways/mission-command/mission-command.gateway.spec.ts
@@ -1,12 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MissionCommandGateway } from './mission-command.gateway';
+import { SubscriptionServiceService } from '@app/services/subscription-service/subscription-service.service';
+import { MissionService } from '@app/services/mission/mission.service';
 
 describe('MissionCommandGateway', () => {
   let gateway: MissionCommandGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MissionCommandGateway],
+      providers: [
+        MissionCommandGateway,
+        {
+          provide: SubscriptionServiceService,
+          useValue: {
+            // Mock any methods used in the MissionCommandGateway
+            robot1: {
+              startMission: jest.fn(),
+              stopMission: jest.fn(),
+              identify: jest.fn(),
+            },
+            robot2: {
+              startMission: jest.fn(),
+              stopMission: jest.fn(),
+              identify: jest.fn(),
+            },
+            gazebo: {
+              startMission: jest.fn(),
+              stopMission: jest.fn(),
+              identify: jest.fn(),
+            },
+            subscribeToTopicRobots: jest.fn(),
+            subscribeToTopicGazebo: jest.fn(),
+          },
+        },
+        {
+          provide: MissionService,
+          useValue: {
+            // Mock any methods used in the MissionCommandGateway
+            createMission: jest.fn(),
+            addLogToMission: jest.fn(),
+            getAllMissions: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     gateway = module.get<MissionCommandGateway>(MissionCommandGateway);
