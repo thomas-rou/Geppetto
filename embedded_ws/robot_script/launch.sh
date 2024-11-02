85ef48716b0db9deaa90acaf904ec6d94eb5b310
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-11-01 21:51:05 -0400

    switch on main branch

--- embedded_ws/robot_script/launch.sh
+++ embedded_ws/robot_script/launch.sh
@@ -1,4 +1,4 @@
-MAIN_BRANCH="feat/start_sim_from_interface"
+MAIN_BRANCH="develop"
 echo "EN ATTENTE" >/tmp/.mission_status
 AUDIO_DEVICE_ID=$(pactl list short sinks | grep "USB_PnP_Audio_Device" | awk '{print $1}')
 pactl set-default-sink $AUDIO_DEVICE_ID
