fa71db0b4d1eb7bf9bbcfe8fca3d8a2900679fab
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-11-12 01:43:34 -0500

    feat: theme stored in local storage

--- application/client/src/app/services/theme/theme.service.ts
+++ application/client/src/app/services/theme/theme.service.ts
@@ -6,12 +6,20 @@ import { BehaviorSubject } from 'rxjs';
     providedIn: 'root',
 })
 export class ThemeService {
-    private isDarkMode: boolean = true;
+    private isDarkMode: boolean;
     private themeSubject = new BehaviorSubject<Theme>(this.getCurrentTheme());
     themeChanged = this.themeSubject.asObservable();
 
+    constructor() {
+        this.isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+        const theme = this.getCurrentTheme();
+        document.documentElement.setAttribute('data', theme);
+        this.themeSubject.next(theme);
+    }
+
     toggleTheme() {
         this.isDarkMode = !this.isDarkMode;
+        localStorage.setItem('isDarkMode', this.isDarkMode.toString());
         const theme = this.getCurrentTheme();
         document.documentElement.setAttribute('data', theme);
         this.themeSubject.next(theme);
