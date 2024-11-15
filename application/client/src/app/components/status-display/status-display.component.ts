2df9ed5a1c8070c3f67730f92b34355129ce0cc8
Thomas R <thomas.rouleau@polymtl.ca>
2024-11-15 16:55:18 -0500

    feat: implement robot pose subscription in status display component for real-time updates

--- application/client/src/app/components/status-display/status-display.component.ts
+++ application/client/src/app/components/status-display/status-display.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RobotManagementService } from '@app/services/robot-management/robot-management.service';
 import { RobotCommunicationService } from '@app/services/robot-communication/robot-communication.service';
 import { RobotId } from '@common/enums/RobotId';
 import { collapseExpandAnimation } from 'src/assets/CollapseExpand';
+import { RobotPose } from '@common/interfaces/RobotPose';
 
 @Component({
     selector: 'app-status-display',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './status-display.component.html',
     styleUrl: './status-display.component.scss',
     animations: [collapseExpandAnimation],
 })
-export class StatusDisplayComponent {
+export class StatusDisplayComponent implements OnInit {
     isCollapsed : boolean = false;
+    robotPoses: { [key: string]: RobotPose[] } = {};
 
     constructor(
         private robotService: RobotCommunicationService,
         private robotManagementService: RobotManagementService,
     ) {}
+
+    ngOnInit(): void {
+        this.robotService.onRobotPositions().subscribe((robotPose: RobotPose) => {
+            if (robotPose.topic) {
+                this.robotPoses[robotPose.topic] = [robotPose];
+            }
+        });
+      }
 
     get robot1() {
         return this.robotManagementService.robot1;
