bb2c127a69327ea8b68513f8e6cf489416950c64
Abdul Chaarani <abdul@chaarani.ca>
2024-11-30 14:01:06 -0500

    feat: namespaced geofence node

--- embedded_ws/src/com_bridge/com_bridge/geofence.py
+++ embedded_ws/src/com_bridge/com_bridge/geofence.py
@@ -9,6 +9,7 @@ from nav2_simple_commander.robot_navigator import BasicNavigator
 from time import sleep
 
 TIME_TO_GATHER_TOUGHTS = 3
+GLOBAL_FRAME_ID = "world"
 
 
 class GeoFenceState(Enum):
@@ -18,9 +19,15 @@ class GeoFenceState(Enum):
 
 
 class GeofenceNode(Node):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("geofence_node")
-        self.navigator = BasicNavigator(namespace="limo1")
+
+        self.declare_parameter("robot_id", "unknown")
+        self.robot_id = (
+            self.get_parameter("robot_id").get_parameter_value().string_value
+        )
+
+        self.navigator = BasicNavigator(namespace=self.robot_id)
         self.get_logger().info("Geofence is up and running!")
 
         self.geofence_x_min = -1.5
@@ -35,22 +42,22 @@ class GeofenceNode(Node):
         self.state = GeoFenceState.INSIDE
 
         self.exploration_publisher = self.create_publisher(
-            Bool, "limo1/explore/resume", GlobalConst.QUEUE_SIZE
+            Bool, f"/{self.robot_id}/explore/resume", GlobalConst.QUEUE_SIZE
         )
 
         self.pose_subscription = self.create_subscription(
             Pose,
-            "/limo1/pose",
+            f"/{self.robot_id}/pose",
             self.pose_callback,
             GlobalConst.QUEUE_SIZE,
         )
 
-    def pose_callback(self, msg):
+    def pose_callback(self, msg: Pose) -> None:
         self.x = msg.position.x
         self.y = msg.position.y
         self.check_state()
 
-    def check_state(self):
+    def check_state(self) -> None:
         match self.state:
             case GeoFenceState.INSIDE:
                 # Check if robot is outside the geofence
@@ -71,7 +78,7 @@ class GeofenceNode(Node):
                     self.resume_exploration()
                     self.state = GeoFenceState.INSIDE
 
-    def is_outside_geofence(self, x: float, y: float):
+    def is_outside_geofence(self, x: float, y: float) -> Bool:
         return (
             x < self.geofence_x_min
             or x > self.geofence_x_max
@@ -79,36 +86,41 @@ class GeofenceNode(Node):
             or y > self.geofence_y_max
         )
 
-    def pause_exploration(self):
+    def pause_exploration(self) -> None:
         self.get_logger().info("Pausing exploration...")
         msg = Bool()
         msg.data = False
         self.exploration_publisher.publish(msg)
 
-    def resume_exploration(self):
+    def resume_exploration(self) -> None:
         sleep(TIME_TO_GATHER_TOUGHTS)
         self.get_logger().info("Resuming exploration...")
         msg = Bool()
         msg.data = True
         self.exploration_publisher.publish(msg)
 
-    def return_to_geofence(self):
+    def return_to_geofence(self) -> None:
         sleep(TIME_TO_GATHER_TOUGHTS)
-        pose = self.create_pose_stamped(0.0, 0.0)
+        pose = self.create_pose_stamped(*self.calculate_geofence_midpoint())
 
         self.navigator.goToPose(pose)
         self.get_logger().info(f"Returning to the geofence")
 
-    def create_pose_stamped(self, target_x: float, target_y: float):
+    def create_pose_stamped(self, target_x: float, target_y: float) -> PoseStamped:
         pose_stamped = PoseStamped()
         pose_stamped.header = Header()
         pose_stamped.header.stamp = Time().to_msg()
-        pose_stamped.header.frame_id = "world"
+        pose_stamped.header.frame_id = GLOBAL_FRAME_ID
 
         pose_stamped.pose = Pose()
         pose_stamped.pose.position = Point(x=target_x, y=target_y, z=0.0)
         pose_stamped.pose.orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
         return pose_stamped
+
+    def calculate_geofence_midpoint(self) -> tuple[float, float]:
+        midpoint_x = (self.geofence_x_min + self.geofence_x_max) / 2
+        midpoint_y = (self.geofence_y_min + self.geofence_y_max) / 2
+        return midpoint_x, midpoint_y
 
 
 def main(args=None):
