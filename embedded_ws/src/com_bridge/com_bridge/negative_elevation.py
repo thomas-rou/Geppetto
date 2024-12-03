70a8d09812f9ca45010dff8fbbd8b27494fa536d
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-12-02 21:16:38 -0500

    firs version of negative elevation

--- embedded_ws/src/com_bridge/com_bridge/negative_elevation.py
+++ embedded_ws/src/com_bridge/com_bridge/negative_elevation.py
@@ -4,13 +4,13 @@ from rclpy.node import Node
 from com_bridge.log import LoggerNode
 from com_bridge.common_enums import GlobalConst, LogType, RobotStatus
 from sensor_msgs.msg import PointCloud2
-
+import numpy as np
 
 class NegativeElevation(Node):
     def __init__(self):
         super().__init__('negative_elevation')
         self.logger = LoggerNode()
-        self.logger.log_message(LogType.INFO, f"negative elevation node launched on {os.getenv('ROBOT')}")
+        self.logger.log_message(LogType.INFO, f"Negative elevation node launched on {os.getenv('ROBOT')}")
 
         self.point_cloud_subscription = self.create_subscription(
             PointCloud2,
@@ -19,14 +19,34 @@ class NegativeElevation(Node):
             GlobalConst.QUEUE_SIZE
         )
         
+        self.fall_threshold = -0.5 
+        
     def status_elevation_callback(self, point_cloud):
         try:
-            # calculate the negative elevation
-            negative_elevation = 0
-            
+            pc_data = self.extract_points_from_cloud(point_cloud)
+
+            negative_elevation = np.min(pc_data)
+            self.logger.log_message(LogType.INFO, f"Negative elevation: {negative_elevation}")
+
+            if negative_elevation < self.fall_threshold:
+                pass
+                # self.logger.log_message(LogType.INFO, "Robot elevation too low! Stopping robot.")
+                # self.stop_robot()
+
         except Exception as e:
             self.logger.log_message(LogType.INFO, "Failed to calculate negative elevation: "+str(e))
-        
+
+    def extract_points_from_cloud(self, point_cloud):
+        pc_data = []
+        for point in pc2.read_points(point_cloud, field_names=("x", "y", "z"), skip_nans=True):
+            _, _, z = point 
+            pc_data.append(z)
+
+        return np.array(pc_data)
+
+    def stop_robot(self):
+        self.logger.log_message(LogType.INFO, "Published stop command to robot.")
+
         
 def main(args=None):
     rclpy.init(args=args)
