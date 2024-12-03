8dad23431d208962213c208419ea7612a1fd7d97
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-12-03 00:10:02 -0500

    ajust treshold

--- embedded_ws/src/com_bridge/com_bridge/negative_elevation.py
+++ embedded_ws/src/com_bridge/com_bridge/negative_elevation.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import PointCloud2
 import sensor_msgs_py.point_cloud2 as pc2
 import numpy as np
 
-FALL_THRESHOLD = 0.8
+FALL_THRESHOLD = 0.5
 MIN_ACCEPTABLE_ELEVATION = 0.3
 
 class NegativeElevation(Node):
