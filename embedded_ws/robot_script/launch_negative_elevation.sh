82e4ff31b94a892039dbf649aa07ce03eda1be9f
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-12-03 01:24:14 -0500

    fix lint error

--- embedded_ws/robot_script/launch_negative_elevation.sh
+++ embedded_ws/robot_script/launch_negative_elevation.sh
@@ -2,4 +2,4 @@ sudo chown nvidia:nvidia /dev/ttyTHS1 &&
 cd ~/geppetto/embedded_ws&& source install/setup.bash &&
 ros2 launch limo_bringup limo_start.launch.py &
 cd ~/ros2_ws && source install/setup.bash && ros2 launch orbbec_camera dabai.launch.py &
-cd ~/geppetto/embedded_ws&& source install/setup.bash && ros2 run com_bridge negative_elevation
+cd ~/geppetto/embedded_ws && source install/setup.bash && ros2 run com_bridge negative_elevation
